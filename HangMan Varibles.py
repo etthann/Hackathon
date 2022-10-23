a876e46d1c3655ac1eb31ab5125056b69f4de1d4
etthann <110430934+etthann@users.noreply.github.com>
2022-10-23 14:38:04 -0400

    Add files via upload

---
+++ HangMan Varibles.py
@@ -0,0 +1,17 @@
+"""variables"""
+
+counter = 0
+word = []
+blank_space = []
+a = []
+#List of games with complicated characters (I'm just lazy)
+b = ['Baldurâ€™s Gate', 'Assassinâ€™s Creed', 'Americaâ€™s Army', 'prisonerâ€™s base', 'blindmanâ€™s buff',
+     'Nine Menâ€™s Morris', 'kho-kho (Indian tag)', 'Bouts-rimÃ©s', 'PokÃ©mon', 'Ã©cartÃ©']
+index_pos_upper = 0
+index_pos_lower = 0
+start = False
+index = []
+indices = []
+length = []
+ActualWord = ''
+capitalize = True
